e9f39bb9438da8927c6c37372fdf9e923c9bb321
Daniel Hara <haradaniel734@gmail.com>
2022-10-12 23:51:32 +0200

    TDD: Must contain a field to search for Power Threshold

--- spec/home.test.tsx
+++ spec/home.test.tsx
@@ -35,50 +35,62 @@ describe('home Page', () => {
     });
   });
 
-  describe('Search functionality', () => {
-    it('contains input where you can search Pokemon', () => {
-      const { getByLabelText } = render(<ListPage pokemons={mockPokemons}/>);
-
-      expect(getByLabelText('Search')).toBeInTheDocument();
+  describe('Filtering', () => {
+    describe('by name', () => {
+      it('contains input where you can search Pokemon', () => {
+        const { getByLabelText } = render(<ListPage pokemons={mockPokemons}/>);
+  
+        expect(getByLabelText('Search')).toBeInTheDocument();
+      });
+  
+      it('searching for "Bulbasaur" will include only the Bulbasaur Pokemon in the result', () => {
+        const { getByLabelText, getByRowgroupType,  } = render(<ListPage pokemons={mockPokemons}/>, { queries: { ...queries, ...tableQueries }}  );
+  
+        fireEvent.change(getByLabelText('Search'), { target: { value: 'Bulbasaur' } });
+  
+        const tbody = getByRowgroupType('tbody');
+        const { getAllRows, getRowByFirstCellText, queryAllRowsByFirstCellText } = within(tbody, { ...queries, ...tableQueries } );
+  
+        expect(getAllRows()).toHaveLength(1);
+        
+        const bulbasaurRow = getRowByFirstCellText('1');
+        expect(bulbasaurRow).toHaveTextContent('Bulbasaur');
+  
+        expect(queryAllRowsByFirstCellText('2')).toHaveLength(0);
+      });
+  
+  
+      it('searching for "char" will include "Charmander", "Charmeleon" and "Charizard"', () => {
+        const { getByLabelText, getByRowgroupType } = render(<ListPage pokemons={mockPokemons}/>, { queries: { ...queries, ...tableQueries }}  );
+  
+        fireEvent.change(getByLabelText('Search'), { target: { value: 'char' } });
+  
+        const tbody = getByRowgroupType('tbody');
+        const { getAllRows, getRowByFirstCellText, queryAllRowsByFirstCellText } = within(tbody, { ...queries, ...tableQueries } );
+  
+        expect(getAllRows()).toHaveLength(3);
+        
+        const charmanderRow = getRowByFirstCellText('4');
+        expect(charmanderRow).toHaveTextContent('Charmander');
+  
+        const charmeleonRow = getRowByFirstCellText('5');
+        expect(charmeleonRow).toHaveTextContent('Charmeleon');
+  
+        const charizardRow = getRowByFirstCellText('6');
+        expect(charizardRow).toHaveTextContent('Charizard');
+  
+        expect(queryAllRowsByFirstCellText('2')).toHaveLength(0);
+      });
     });
 
-    it('searching for "Bulbasaur" will include only the Bulbasaur Pokemon in the result', () => {
-      const { getByLabelText, getByRowgroupType,  } = render(<ListPage pokemons={mockPokemons}/>, { queries: { ...queries, ...tableQueries }}  );
-
-      fireEvent.change(getByLabelText('Search'), { target: { value: 'Bulbasaur' } });
-
-      const tbody = getByRowgroupType('tbody');
-      const { getAllRows, getRowByFirstCellText, queryAllRowsByFirstCellText } = within(tbody, { ...queries, ...tableQueries } );
-
-      expect(getAllRows()).toHaveLength(1);
-      
-      const bulbasaurRow = getRowByFirstCellText('1');
-      expect(bulbasaurRow).toHaveTextContent('Bulbasaur');
-
-      expect(queryAllRowsByFirstCellText('2')).toHaveLength(0);
-    });
-
-
-    it('searching for "char" will include "Charmander", "Charmeleon" and "Charizard"', () => {
-      const { getByLabelText, getByRowgroupType } = render(<ListPage pokemons={mockPokemons}/>, { queries: { ...queries, ...tableQueries }}  );
-
-      fireEvent.change(getByLabelText('Search'), { target: { value: 'char' } });
-
-      const tbody = getByRowgroupType('tbody');
-      const { getAllRows, getRowByFirstCellText, queryAllRowsByFirstCellText } = within(tbody, { ...queries, ...tableQueries } );
-
-      expect(getAllRows()).toHaveLength(3);
-      
-      const charmanderRow = getRowByFirstCellText('4');
-      expect(charmanderRow).toHaveTextContent('Charmander');
-
-      const charmeleonRow = getRowByFirstCellText('5');
-      expect(charmeleonRow).toHaveTextContent('Charmeleon');
-
-      const charizardRow = getRowByFirstCellText('6');
-      expect(charizardRow).toHaveTextContent('Charizard');
-
-      expect(queryAllRowsByFirstCellText('2')).toHaveLength(0);
+    describe('by power threshold', () => {
+      it('contains input where you can search a number for Power Threshold', () => {
+        const { getByLabelText } = render(<ListPage pokemons={mockPokemons}/>);
+  
+        const powerThresholdInput = getByLabelText('Power threshold');
+        expect(powerThresholdInput).toBeInTheDocument();
+        expect(powerThresholdInput).toHaveAttribute('type', 'number');
+      });
     });
   });
 });
