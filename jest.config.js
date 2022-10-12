ebd3838e26fbe94ca8f6a4769581b099b9e8297a
Daniel Hara <haradaniel734@gmail.com>
2022-10-12 22:20:23 +0200

    TDD: Must show a pokemon in the list

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
-const nextJest = require("next/jest");
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-  dir: "./",
+  dir: './',
 });
 
 const customJestConfig = {
-  moduleDirectories: ["node_modules", "<rootDir>/"],
-  testEnvironment: "jest-environment-jsdom",
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['./spec/setupTests.ts']
 };
 
 module.exports = createJestConfig(customJestConfig);
