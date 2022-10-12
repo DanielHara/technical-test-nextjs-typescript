12842767bbea073934d313cd43d9a94ad2989814
Daniel Hara <haradaniel734@gmail.com>
2022-10-13 01:55:24 +0200

    TDD: Make tests pass

--- apps/list/Row.tsx
+++ apps/list/Row.tsx
@@ -1,33 +1,39 @@
 import type { Pokemon } from '../../interfaces/pokemon';
 
-const Row = ({id, name, type, hp, attack, defense, special_attack, special_defense, speed}: Pokemon) => <tr>
-  <td>
-    {id}
-  </td>
-  <td>
-    {name}
-  </td>
-  <td>
-    {type.join(', ')}
-  </td>
-  <td>
-    {hp}
-  </td>
-  <td>
-    {attack}
-  </td>
-  <td>
-    {defense}
-  </td>
-  <td>
-    {special_attack}
-  </td>
-  <td>
-    {special_defense}
-  </td>
-  <td>
-    {speed}
-  </td>
-</tr>;
+const Row = ({id, name, type, hp, attack, defense, special_attack, special_defense, speed}: Pokemon) => {
+  const handleClick = () => {
+    window.open(`/pokemon/${id}`);
+  };
+
+  return  <tr onClick={handleClick}>
+    <td>
+      {id}
+    </td>
+    <td>
+      {name}
+    </td>
+    <td>
+      {type.join(', ')}
+    </td>
+    <td>
+      {hp}
+    </td>
+    <td>
+      {attack}
+    </td>
+    <td>
+      {defense}
+    </td>
+    <td>
+      {special_attack}
+    </td>
+    <td>
+      {special_defense}
+    </td>
+    <td>
+      {speed}
+    </td>
+  </tr>;
+};
 
 export default Row;
